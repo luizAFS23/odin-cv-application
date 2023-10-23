b5e806d2bf21f078792b92a4efc0def628f8ff41
luizAFS23 <luizafs.1500@gmail.com>
2023-10-23 18:20:08 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,27 @@ function Header(){
   )
 }
 
+function Main(){
+  return(
+    <>
+      <h1>Congratulations, you've just stepped foot into an awesome way of getting a job!</h1>
+      <h2>First of all, please put on your information here (it will take a few seconds):</h2>
+      <form action="">
+        <label htmlFor="name">Name:</label>
+        <input type="text" name='name' placeholder='Your name here'/>
+        <label htmlFor="">Age:</label>
+        
+        <button type='submit'>Submit Information</button>
+      </form>
+    </>
+  )
+}
 
 function App() {
   return (
     <>
       <Header/>
-      
+      <Main/>
     </>
   )
 }
