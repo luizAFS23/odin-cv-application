a0c42889810b720c32d95f8887e31223bfa5e8a9
luizAFS23 <luizafs.1500@gmail.com>
2023-10-23 18:25:23 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,16 @@ function GeneralInformation(){
     <>
       <h1>Congratulations, you've just stepped foot into an awesome way of getting a job!</h1>
       <h2>First of all, please put on some general information (it will take a few seconds):</h2>
-      <form action="">
-        <label htmlFor="name">Name:</label>
+      <form action="post" name='general-information-form'>
+        <label htmlFor="name">Enter your name:</label>
         <input type="text" name='name' placeholder='Your name here'/><br/>
-        <label htmlFor="age">Age:</label>
-        <input type="number" name='age'/><br/>
+
+        <label htmlFor="email">Enter an email:</label>
+        <input type="email" name='email' placeholder='Your email here'/><br/>
+
+        <label htmlFor="password">Enter a new password:</label>
+        <input type="password" name='password' placeholder='Your new password here'/><br/>
+
         <button type='submit'>Submit Information</button>
       </form>
     </>
