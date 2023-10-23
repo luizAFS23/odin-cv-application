6a9b0e3c62d1de9afb321bfc9362b83f0958feac
luizAFS23 <luizafs.1500@gmail.com>
2023-10-23 18:49:50 -0300

    changes

--- src/components/EmailTaker.jsx
+++ src/components/EmailTaker.jsx
@@ -1,11 +1,11 @@
-export default function NameTaker(props) {
+export default function EmailTaker(props) {
     return(
       <div>
-        <p>Please enter your name.</p>
+        <p>Please enter your email.</p>
         <form onSubmit={props.onSubmit}>
           <label>
             Name:
-            <input type='text' name='name' />
+            <input type='email' name='email' />
           </label>
           <button type='submit'>Submit</button>
         </form>
