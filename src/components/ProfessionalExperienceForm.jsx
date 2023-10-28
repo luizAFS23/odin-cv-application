585f3c58208d2e7aebac52d6a70c98893f251e2f
luizAFS23 <luizafs.1500@gmail.com>
2023-10-28 09:37:32 -0300

    changes

--- src/components/ProfessionalExperienceForm.jsx
+++ src/components/ProfessionalExperienceForm.jsx
@@ -2,7 +2,6 @@ export default function ProfessionalExperienceForm(){
     return(
         <div>
             <h1>Almost there! Now, please tell more about your professional experience: </h1>
-            
         </div>
     )
 }
